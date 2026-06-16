5fd5dc7aa8d3d03f02d6c36195466c8df6a985f5
yh2237 <yh@0yh.dev>
2026-06-17 07:22:40 +0900

    logs

--- start.sh
+++ start.sh
@@ -9,8 +9,8 @@ pm2 start server.js \
     --name api-server \
     --log-date-format "YYYY-MM-DD HH:mm:ss" \
     --update-env \
-    --output log/pm2-out.log \
-    --error log/pm2-err.log
+    --output logs/pm2-out.log \
+    --error logs/pm2-err.log
 
 echo "pm2: api-server started"
 pm2 status api-server
